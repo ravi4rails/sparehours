d108b3fd7cbb7a0d58a142fd302d982b5962c704
ravi4rails <ravionrails@gmail.com>
2019-04-20 14:01:03 +0530

    fixed lat and long generation

--- app/assets/javascripts/autocomplete.js
+++ app/assets/javascripts/autocomplete.js
@@ -1,40 +1,23 @@
 var placeSearch, autocomplete;
-
 var componentForm = {
   locality: 'long_name',
   administrative_area_level_1: 'short_name',
   country: 'long_name'
 };
-
 function initAutocomplete() {
-  // Create the autocomplete object, restricting the search predictions to
-  // geographical location types.
   autocomplete = new google.maps.places.Autocomplete(
-    document.getElementById('autocomplete'), {types: ['geocode']});
-
-  // Avoid paying for data that you don't need by restricting the set of
-  // place fields that are returned to just the address components.
-  autocomplete.setFields(['address_component']);
-
-  // When the user selects an address from the drop-down, populate the
-  // address fields in the form.
+      (document.getElementById('autocomplete')),
+      {types: ['geocode']});
   autocomplete.addListener('place_changed', fillInAddress);
 }
-
 function fillInAddress() {
-  // Get the place details from the autocomplete object.
   var place = autocomplete.getPlace();
-  
-  $("#latitude").val(results[0].geometry.location.lat());
-  $("#longitude").val(results[1].geometry.location.lng());
-
+  $("#latitude").val(place.geometry.location.lat());
+  $("#longitude").val(place.geometry.location.lng());
   for (var component in componentForm) {
     document.getElementById(component).value = '';
     document.getElementById(component).disabled = false;
   }
-
-  // Get each component of the address from the place details,
-  // and then fill-in the corresponding field on the form.
   for (var i = 0; i < place.address_components.length; i++) {
     var addressType = place.address_components[i].types[0];
     if (componentForm[addressType]) {
@@ -43,9 +26,6 @@ function fillInAddress() {
     }
   }
 }
-
-// Bias the autocomplete object to the user's geographical location,
-// as supplied by the browser's 'navigator.geolocation' object.
 function geolocate() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
@@ -53,8 +33,10 @@ function geolocate() {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
-      var circle = new google.maps.Circle(
-        {center: geolocation, radius: position.coords.accuracy});
+      var circle = new google.maps.Circle({
+        center: geolocation,
+        radius: position.coords.accuracy
+      });
       autocomplete.setBounds(circle.getBounds());
     });
   }
