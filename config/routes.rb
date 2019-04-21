ccfaa41e28c2632b14144f61d050f8b5d1faeec1
ravi4rails <ravionrails@gmail.com>
2019-04-21 23:06:34 +0530

    added skills

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :skills
   root "home#index"
   devise_for :users, controllers: {
     sessions: 'users/sessions',
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_19_094210) do
+ActiveRecord::Schema.define(version: 2019_04_21_170817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "skills", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "taggings", id: :serial, force: :cascade do |t|
     t.integer "tag_id"
